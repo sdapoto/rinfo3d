65e56e718965d39a7fa2b25247c4651f4e898ad8
fedecris <federicocristina@gmail.com>
2015-05-01 16:55:47 -0300

    Instanciacion dinamica de ciudad, calles y avenidas.  Robot simple para puebas iniciales.

--- Assets/Scripts/Init.cs
+++ Assets/Scripts/Init.cs
@@ -3,23 +3,38 @@ using System.Collections;
 
 public class Init : MonoBehaviour {
 
-	// Numero de calles y avenidas
+	// CONSTANTES
 	public static int CANT_CALLES = 100;
 	public static int CANT_AVENIDAS = 100;
+	public static float ELEVACION_CIUDAD = -0.01f;
+	public static float ELEVACION_CALLEAV = 0.01f;
+	public static float ANCHO_CALLEAV = 0.25f;
 
-	// Prefab calles y avenidas para inicializar ciudad 100x100
+	// Prefab ciudad, calles y avenidas para inicializar ciudad
+	public Transform ciudadPrefab;
 	public Transform callePrefab; 
 	public Transform avenidaPrefab; 
 
 	// Use this for initialization
 	void Start () {
-		// Inicializar calles y avenidas
+		// Inicializar ciudad
+		Object ciudadInstance = Instantiate (
+				ciudadPrefab, 
+				new Vector3 (CANT_AVENIDAS / 2 + .5f, ELEVACION_CIUDAD, CANT_CALLES / 2 + .5f ), 
+				Quaternion.identity
+			);
+		((Transform)ciudadInstance).localScale = new Vector3 (CANT_AVENIDAS / 10, 0.01f, CANT_CALLES / 10);
+
+		// Inicializar calles
 		for (int z = 1; z <= CANT_CALLES; z++) {
-			Instantiate(callePrefab, new Vector3(50.5f, 0f, (float)z), Quaternion.identity);
+			Object calleInstance = Instantiate(callePrefab, new Vector3(CANT_CALLES/2 + .5f, ELEVACION_CALLEAV, (float)z), Quaternion.identity);
+			((Transform)calleInstance).localScale = new Vector3 (CANT_CALLES - 1, ELEVACION_CALLEAV, ANCHO_CALLEAV);
 		}
-		// Inicializar calles y avenidas
+
+		// Inicializar avenidas
 		for (int x = 1; x <= CANT_AVENIDAS; x++) {
-			Instantiate(avenidaPrefab, new Vector3((float)x, 0f, 50.5f), Quaternion.identity);
+			Object avenidaInstance = Instantiate(avenidaPrefab, new Vector3((float)x, ELEVACION_CALLEAV, CANT_AVENIDAS/2 + .5f), Quaternion.identity);
+			((Transform)avenidaInstance).localScale = new Vector3 (ANCHO_CALLEAV, ELEVACION_CALLEAV, CANT_AVENIDAS - 1);
 		}
 
 	}
---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI : MonoBehaviour {
+
+	// Posibles estados de la GUI
+	public const int STATE_EDITING = 0;
+	public const int STATE_RUNNING = 1;
+	public const int STATE_CONFIG  = 2;
+	
+	// Estado actual de la GUI. Inicia en EDITING logicamente
+	public static int currentState = STATE_EDITING;
+
+	void OnGUI() { 
+		switch (currentState) { 	
+			case STATE_EDITING: {
+				renderEditing();
+				break;
+			}
+			case STATE_RUNNING: {
+				renderRunning();
+				break;
+			}
+			case STATE_CONFIG: {
+				renderConfig();
+				break;
+			}
+		}
+
+
+
+	}
+
+	/** Renders the Editing Menu */
+	void renderEditing() {
+
+	}
+
+	/** Renders the Running Menu */
+	void renderRunning() {
+		
+	}
+
+	/** Renders the Config Menu */
+	void renderConfig() {
+		
+	}
+
+}
