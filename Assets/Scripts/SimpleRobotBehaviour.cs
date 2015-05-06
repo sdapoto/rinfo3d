0d2dfd4f5a44f7786f47390c61f336cac2cf65ef
sdapoto <sdapoto@lidi.info.unlp.edu.ar>
2015-05-06 15:33:02 -0300

    Agregando behaviour

---
+++ Assets/Scripts/SimpleRobotBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleRobotBehaviour : RobotBehaviour {
+
+	/**
+	 * Metodo a implementar
+	 */
+	public override void mover()
+	{
+
+	}
+	
+	/**
+	 * Metodo a implementar
+	 */
+	public override void rotarIzq()
+	{
+
+	}	
+
+
+}
